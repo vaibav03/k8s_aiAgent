9174f8d24f385987ea3263ebe0138d922e4e8cb2
nirmalkanniaiyan <nirmalkanniaiyan1729@gmail.com>
2025-04-20 14:30:21 +0530

    mem check

--- exp.js
+++ exp.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const podName = "finalpod-77c649c5fc-tzvnb"
-const res = await axios.get(`http://localhost:8001/api/v1/namespaces/default/pod/${podName}`);
+// const podName = "finalpod-77c649c5fc-tzvnb"
+const podName = "finalpod-64ff6f79c5-vdtgt"
+
+const res = await axios.get(`http://localhost:8001/api/v1/namespaces/default/pods/${podName}`);
 console.log(res)
